211320ed34e846d2d4b1178e6e9a648df1433feb
Omodot-Etukudo <omodote@hotmail.com>
2022-09-15 10:17:26 +0100

    update:fix card mq

--- components/Mediacard.js
+++ components/Mediacard.js
@@ -7,7 +7,7 @@ function Mediacard({picture, title, artist, isplaylist, isPodcast,id}) {
     return (
         <Link passHref  href="/album/id">
             <div className="group h-54">
-                <div className=" cursor-pointer w-32 lg:w-38 lg:dark:bg-card-bg lg:dark:group-hover:bg-card-hover bg-gray-100 lg:group-hover:bg-gray-200 lg:shadow-xl bg-opacity-50 lg:px-4 pt-4 pb-5 flex flex-col rounded transition-colors duration-500">
+                <div className=" cursor-pointer w-32 lg:w-38 lg:dark:bg-card-bg lg:dark:group-hover:bg-card-hover bg-gray-100 lg:group-hover:bg-gray-200 lg:shadow-xl bg-opacity-50 lg:px-4 pt-4 pb-5 flex flex-col rounded transition-colors duration-500 dark:border-0 border border-gray-100">
                     <div className="-z-10 lg:z-auto"><Image alt={title} placeholder="blur" blurDataURL loading="lazy" className="lg:rounded lg:flex lg:shadow-2xl lg:drop-shadow-2xl" width={220} height={220}  src={`${BASE_URL}${picture}` || `${BASE_URL2}${picture}` || `${BASE_URL3}${picture}`} /></div>
                     <div className="title-text">
                         <h3 className="text-xs lg:text-xs mt-2 lg:mt-4 font-semibold dark:text-white text-main-bg lg:text-left text-center truncate">{title}</h3>
